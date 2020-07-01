52b9606bfe45bf90de48e252e11d1186286f9b08
sriyegna <sriyegna@users.noreply.github.com>
2020-07-01 15:51:56 -0400

    Added routing

--- src/Container/AppContainer/AppContainer.js
+++ src/Container/AppContainer/AppContainer.js
@@ -1,16 +1,11 @@
 import React from "react";
 
 import AppDrawer from "../../Container/AppDrawer/AppDrawer";
-import DayGrid from "../../Components/FullCalendar/DayGrid";
-import ListGrid from "../../Components/FullCalendar/ListGrid";
 
 const AppContainer = () => {
   return (
     <>
-      <AppDrawer>
-        <ListGrid />
-        <DayGrid />
-      </AppDrawer>
+      <AppDrawer />
     </>
   );
 };
--- src/Container/AppDrawer/AppDrawer.js
+++ src/Container/AppDrawer/AppDrawer.js
@@ -20,6 +20,11 @@ import MailIcon from "@material-ui/icons/Mail";
 import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
 import ListIcon from "@material-ui/icons/List";
 
+import DayGrid from "../../Components/FullCalendar/DayGrid";
+import ListGrid from "../../Components/FullCalendar/ListGrid";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
@@ -98,84 +103,102 @@ const AppDrawer = (props) => {
   };
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open,
-            })}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Mini variant drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: clsx({
+    <Router>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, {
+                [classes.hide]: open,
+              })}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              Mini variant drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          className={clsx(classes.drawer, {
             [classes.drawerOpen]: open,
             [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          <ListItem button key="listView">
-            <ListItemIcon>
-              <ListIcon />
-            </ListItemIcon>
-            <ListItemText primary="List View" />
-          </ListItem>
-          <ListItem button key="calendarView">
-            <ListItemIcon>
-              <CalendarTodayIcon />
-            </ListItemIcon>
-            <ListItemText primary="Calendar View" />
-          </ListItem>
-        </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <main className={classes.content}>
-        <div className="Calendar">{props.children}</div>
-      </main>
-    </div>
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open,
+            }),
+          }}
+        >
+          <div className={classes.toolbar}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "rtl" ? (
+                <ChevronRightIcon />
+              ) : (
+                <ChevronLeftIcon />
+              )}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            <Link to="/listgrid">
+              <ListItem button key="listView">
+                <ListItemIcon>
+                  <ListIcon />
+                </ListItemIcon>
+                <ListItemText primary="List View" />
+              </ListItem>
+            </Link>
+            <Link to="/calendargrid">
+              <ListItem button key="calendarView">
+                <ListItemIcon>
+                  <CalendarTodayIcon />
+                </ListItemIcon>
+                <ListItemText primary="Calendar View" />
+              </ListItem>
+            </Link>
+          </List>
+          <Divider />
+          <List>
+            {["All mail", "Trash", "Spam"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className="Calendar">
+            <Switch>
+              <Route exact path="/">
+                <DayGrid />
+              </Route>
+              <Route exact path="/listgrid">
+                <ListGrid />
+              </Route>
+              <Route exact path="/calendargrid">
+                <DayGrid />
+              </Route>
+            </Switch>
+          </div>
+        </main>
+      </div>
+    </Router>
   );
 };
 
